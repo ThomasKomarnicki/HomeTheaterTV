ffb38d8c5b8e4d73de314b2446176df55449b71d
Thomas Komarnicki <komarnicki.thomas@gmail.com>
2016-05-21 10:52:14 -0400

    move discover task to rx java

--- app/src/main/java/com/doglandia/hometheater/clientdiscovery/ClientDiscoverer.java
+++ app/src/main/java/com/doglandia/hometheater/clientdiscovery/ClientDiscoverer.java
@@ -20,6 +20,13 @@ import java.util.concurrent.TimeUnit;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import rx.Observable;
+import rx.Scheduler;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action0;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by tdk10 on 2/22/2016.
@@ -42,11 +49,9 @@ public class ClientDiscoverer {
 
     private OkHttpClient client;
 
-    private boolean found = false;
-
     private boolean cancel = false;
 
-    private int discoveryTasksRunning = 0;
+    private boolean found = false;
 
     public ClientDiscoverer(Context context, OnHostFoundListener onHostFoundListener){
         this.context = context;
@@ -98,89 +103,87 @@ public class ClientDiscoverer {
     }
 
     private void startSubNetScan(){
+        final String subnet = getSubNet(context);
         found = false;
 
-        String subnet = getSubNet(context);
+        Observable<String> observable = scanSubnetObservable(subnet, 1, 127)
+                .mergeWith(scanSubnetObservable(subnet, 128, 255));
 
-        discoveryTasksRunning = 0;
-        DiscoveryTask discoveryTask = new DiscoveryTask();
-        // scan subnet
-        discoveryTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, subnet,"1-127");
-        discoveryTask = new DiscoveryTask();
-        discoveryTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, subnet,"128-255");
-        discoveryTasksRunning +=2;
-
-        // also scan other subnet? one for hard wire one for wifi?
-        discoveryTask = new DiscoveryTask();
         if(subnet.equals("192.168.1.")){
-            discoveryTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "192.168.0.","1-127");
-            discoveryTask = new DiscoveryTask();
-            discoveryTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "192.168.0.","128-255");
+            observable = observable.mergeWith(scanSubnetObservable("192.168.0.", 1, 127)
+                    .mergeWith(scanSubnetObservable("192.168.0.", 128, 255)));
         }else if(subnet.equals("192.168.0.")){
-            discoveryTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "192.168.1.","1-127");
-            discoveryTask = new DiscoveryTask();
-            discoveryTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "192.168.1.","128-255");
+            observable = observable.mergeWith(scanSubnetObservable("192.168.1.", 1, 127)
+                    .mergeWith(scanSubnetObservable("192.168.1.", 128, 255)));
         }
-        discoveryTasksRunning +=2;
+
+        observable
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String host) {
+                        found = true;
+//                        cancel = true;
+                        Log.d(TAG, "On Next " + host);
+                        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+                        prefs.edit().putString(LAST_DISCOVERED_HOST, host).commit();
+                        listener.onHostFound(host);
+
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        throwable.printStackTrace();
+                    }
+                }, new Action0() {
+                    @Override
+                    public void call() {
+                        // on complete, error?
+                        if(!found) {
+                            listener.onNoHostFound();
+                            HomeTheaterApplication.getBus().post(new ResourceServerConnectFailed());
+                        }
+                    }
+                });
+
     }
 
-    // scans subnet for appropriate client
-
-    class DiscoveryTask extends AsyncTask<String, Integer, String>{
-
-        @Override
-        protected String doInBackground(String... params) {
-
-            String[] range = params[1].split("-");
-            return scanSubNet(params[0],Integer.valueOf(range[0]),Integer.valueOf(range[1])); // todo
-        }
-
-        @Override
-        protected void onPostExecute(String host) {
-            super.onPostExecute(host);
-            if(host != null) {
-                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-                prefs.edit().putString(LAST_DISCOVERED_HOST, host).commit();
-                found = true;
-                listener.onHostFound(host);
-            }else if(!found){
-                listener.onNoHostFound();
+    private Observable<String> scanSubnetObservable(final String subnet, final int startAddress, final int endAddress){
+        return Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                scanSubNet(subnet,startAddress,endAddress, subscriber);
+                subscriber.onCompleted();
             }
-            discoveryTasksRunning--;
+        }).subscribeOn(Schedulers.newThread());
+    }
 
-            if(discoveryTasksRunning == 0 && !cancel){
-                // todo not connected
-                HomeTheaterApplication.getBus().post(new ResourceServerConnectFailed());
+
+    private String scanSubNet(String subnet, int startAddress, int endAddress, Subscriber subscriber){
+        InetAddress inetAddress = null;
+        for(int i=startAddress; i <= endAddress; i++){
+            if(found || cancel){
+                subscriber.onCompleted();
+                return null;
             }
-        }
-
-        private String scanSubNet(String subnet, int startAddress, int endAddress){
-            InetAddress inetAddress = null;
-            for(int i=startAddress; i <= endAddress; i++){
-                if(found || cancel){
-                    cancel(true);
-                    return null;
-                }
 //                Log.d(TAG, "Trying: " + subnet + String.valueOf(i));
-                try {
-                    inetAddress = InetAddress.getByName(subnet + String.valueOf(i));
+            try {
+                inetAddress = InetAddress.getByName(subnet + String.valueOf(i));
 //                    if(inetAddress.isReachable(200)){
 //                        Log.d(TAG, inetAddress.getHostName() + "is reachable");
-                        if(hostAcceptsRequest(inetAddress.getHostName())){
-                            return inetAddress.getHostName();
-                        }
-//                    }
-                } catch (UnknownHostException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    Log.d(TAG, "IO exception, failed on address "+inetAddress.getHostName());
-//                    e.printStackTrace();
+                if(hostAcceptsRequest(inetAddress.getHostName())){
+                    subscriber.onNext(inetAddress.getHostName());
+                    return inetAddress.getHostName();
                 }
+//                    }
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                Log.d(TAG, "IO exception, failed on address "+inetAddress.getHostName());
+//                    e.printStackTrace();
             }
-
-            return null;
         }
-
+        return null;
     }
 
     // checks to make sure client still exists, if not start scanning the subnet
